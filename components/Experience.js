0f48ad12c6c7e36a008c983a72115a59f8f80106
sazalislam04 <mdsazalislam04@gmail.com>
2023-01-10 03:09:19 +0600

    added education

--- components/Experience.js
+++ components/Experience.js
@@ -9,7 +9,7 @@ const Experience = () => {
             className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
             style={{ left: "50%" }}
           ></div>
-          <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+          <div className="mb-4 flex justify-between flex-row-reverse items-center w-full left-timeline">
             <div className="order-1 w-5/12"></div>
             <div className="z-20 flex items-center order-1 bg-gray-800 custom-shadow w-8 h-8 rounded-full">
               <span className="mx-auto text-white font-semibold text-lg">
@@ -19,28 +19,33 @@ const Experience = () => {
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className="w-5 h-5 text-white"
+                  className="w-5 h-5"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
+                    d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
                   />
                 </svg>
               </span>
             </div>
             <div className="order-1 bg-indigo-400 rounded-lg custom-shadow w-5/12 px-6 py-4">
-              <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
+              <h3 className="mb-3 font-bold flex justify-between items-center text-white text-xl">
+                Complete Web Development
+                <span className="text-xs bg-white text-indigo-800 px-2 rounded-full py-1">
+                  Course
+                </span>
+              </h3>
+              <p className="text-sm font-medium mb-2 leading-snug tracking-wide text-white text-opacity-100">
+                Duration: 01 June 2022 to 01 December 2022
+              </p>
               <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                Institute: Programming Hero
               </p>
             </div>
           </div>
 
-          <div className="mb-8 flex justify-between items-center w-full right-timeline">
+          <div className="mb-4 flex justify-between items-center w-full right-timeline">
             <div className="order-1 w-5/12"></div>
             <div className="z-20 flex items-center order-1 bg-gray-800 custom-shadow w-8 h-8 rounded-full">
               <span className="mx-auto text-white font-semibold text-lg">
@@ -50,30 +55,33 @@ const Experience = () => {
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className="w-5 h-5 text-white"
+                  className="w-5 h-5"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
+                    d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
                   />
                 </svg>
               </span>
             </div>
             <div className="order-1 bg-white rounded-lg custom-shadow w-5/12 px-6 py-4">
               <h3 className="mb-3 font-bold text-gray-800 text-xl">
-                Lorem Ipsum
+                Full Stack Web Development Internship
               </h3>
-              <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+              <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100 capitalize">
+                Company: Uptal (brand under Betterplus Technologies Pvt. Ltd)
+              </p>
+              <p className="text-sm leading-snug tracking-wide mt-2 text-gray-900 text-opacity-100 capitalize">
+                Starting: 10th of December 2022 to continue
+              </p>
+              <p className="text-sm leading-snug tracking-wide mt-2 text-gray-900 text-opacity-100 capitalize">
+                Location: Electronic city, Bangalore, IN
               </p>
             </div>
           </div>
 
-          <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+          {/* <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
             <div className="order-1 w-5/12"></div>
             <div className="z-20 flex items-center order-1 bg-gray-800 custom-shadow w-8 h-8 rounded-full">
               <span className="mx-auto text-white font-semibold text-lg">
@@ -83,12 +91,12 @@ const Experience = () => {
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className="w-5 h-5 text-white"
+                  className="w-5 h-5"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
+                    d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
                   />
                 </svg>
               </span>
@@ -102,7 +110,7 @@ const Experience = () => {
                 of type and scrambled it to make a type specimen book.
               </p>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
